eb98e2869d57140fe48a79bb360f564200760dd3
Baran Demirbaş <bardemirbas@gmail.com>
2021-02-01 20:51:09 +0300

    Update URL Scheme

--- browser.go
+++ browser.go
@@ -7,24 +7,15 @@ import (
 	"os/exec"
 	"runtime"
 	"strconv"
-	"strings"
 )
 
 // Browser function opens a new browser tab for pointing url with default browser.
 func Browser(URL string, PORT ...int) {
 	var err error
-	var scheme = []string{"http://", "https://"}
-	var isURL = false
-	for _, scheme := range scheme {
-		if strings.Contains(URL, scheme) {
-			isURL = true
-			break
-		}
-	}
-	if !isURL {
-		URL = "http://" + URL
-	}
 	u, _ := url.Parse(URL)
+	if u.Scheme == "" {
+		u, _ = url.Parse("http://" + URL)
+	}
 	if PORT != nil {
 		URL = u.Scheme + "://" + u.Host + ":" + strconv.Itoa(PORT[0]) + u.Path
 	}
